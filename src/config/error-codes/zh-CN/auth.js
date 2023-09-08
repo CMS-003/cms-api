9bbb71508e2af928f40abc4f2ed9d23c91f89f66
阮家友 <1439120442@qq.com>
2023-09-08 18:31:35 +0800

    修改status

--- src/config/error-codes/zh-CN/auth.js
+++ src/config/error-codes/zh-CN/auth.js
@@ -1,26 +1,26 @@
 module.exports = {
   tokenNotFound: {
-    status: 400,
+    status: 200,
     code: 101000,
     message: 'token未找到!'
   },
   tokenExpired: {
-    status: 400,
+    status: 200,
     code: 101010,
     message: 'token已过期!'
   },
   tokenFail: {
-    status: 400,
+    status: 200,
     code: 101020,
     message: 'token错误!'
   },
   NoPermission: {
-    status: 400,
+    status: 200,
     code: 101030,
     message: '权限不足!'
   },
   VerifyError: {
-    status: 400,
+    status: 200,
     code: 101070,
     message: '验证码错误!'
   },
