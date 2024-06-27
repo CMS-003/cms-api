30944ccdd728bf180945074a1dffeead7356cd32
阮家友 <1439120442@qq.com>
2024-06-27 17:56:28 +0800

    google auth

--- src/services/sns.js
+++ src/services/sns.js
@@ -9,15 +9,32 @@ const superagent = require('superagent');
 require('superagent-proxy')(superagent);
 const { google } = require('googleapis');
 
+const scopes = [
+  'https://www.googleapis.com/auth/userinfo.profile',
+  'https://www.googleapis.com/auth/youtube.readonly',
+  'https://www.googleapis.com/auth/yt-analytics-monetary.readonly',
+  'https://www.googleapis.com/auth/yt-analytics.readonly',
+];
+
 module.exports = {
   signin: (type) => {
     const sns_config = config[type];
     if (!sns_config) {
       return '/404'
     }
+    console.log(type ,process.env.HTTP_PROXY)
     if (type === 'sns_github') {
       return `https://github.com/login/oauth/authorize?client_id=${sns_config.client_id}&scope=user:email`
-    } else {
+    } else if (type === 'sns_google') {
+      const oauth2Client = new google.auth.OAuth2(sns_config.value.client_id, sns_config.value.client_secret, sns_config.value.redirect_uris[0]);
+      const authUrl = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: scopes,
+        include_granted_scopes: true
+      });
+      return authUrl
+    }
+    else {
       return '/404'
     }
   },
@@ -74,11 +91,42 @@ module.exports = {
       return ctx.redirect(config.page_public_url + '/oauth/success?access_token=' + token)
     } else if (type === 'sns_google') {
       const code = ctx.query.code;
-      const youtube_config = config.social.google.web_json;
       // 创建 OAuth2 客户端
-      const oauth2Client = new google.auth.OAuth2(youtube_config.client_id, youtube_config.client_secret, youtube_config.redirect_uris[0]);
+      const oauth2Client = new google.auth.OAuth2(sns_config.value.client_id, sns_config.value.client_secret, sns_config.value.redirect_uris[0]);
       let { tokens } = await oauth2Client.getToken(code);
       logger.info('youtube 授权', JSON.stringify(tokens))
+      const oauth2 = google.oauth2({ auth: oauth2Client, version: 'v2' });
+      const user_resp = await oauth2.userinfo.get()
+      const sns_info = {
+        sns_id: user_resp.data.id,
+        sns_type: 'google',
+        access_token: tokens.access_token,
+        refresh_token: tokens.refresh_token,
+        nickname: user_resp.data.name,
+        avatar: user_resp.data.picture,
+        status: 1,
+        detail: user_resp.data,
+        access_expired_at: new Date(tokens.expiry_date),
+        refresh_expired_at: new Date('2050-01-01'),
+      }
+      const user_id = v4();
+      const r = await BLL.snsBLL.model.updateOne({ sns_id: sns_info.sns_id, sns_type: sns_info.sns_type }, { $set: sns_info, $setOnInsert: { user_id } }, { upsert: true });
+      if (r.upsertedCount === 1) {
+        await BLL.userBLL.model.updateOne({ _id: user_id }, {
+          $set: {
+            account: random(6, 'ichar'),
+            nickname: sns_info.nickname,
+            avatar: sns_info.avatar,
+            pass: '',
+            salt: randomstring.generate({ length: 10, charset: 'hex' }),
+            status: 1
+          }
+        }, { upsert: true });
+      }
+      const sns = await BLL.snsBLL.getInfo({ where: { sns_id: sns_info.sns_id, sns_type: sns_info.sns_type }, lean: true });
+      const user = await BLL.userBLL.getInfo({ where: { _id: sns.user_id }, lean: true });
+      const token = await userService.genToken(user)
+      return ctx.redirect(config.page_public_url + '/oauth/success?access_token=' + token)
     } else {
       return ctx.redirect(config.page_public_url + '/oauth/fail')
     }
