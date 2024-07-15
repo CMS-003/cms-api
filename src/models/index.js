e5f176126da8285606433dd2f695cc85e2e7f305
max <ruanjiayou123@gmail>
2024-07-16 01:28:31 +0800

    test schema

--- src/models/index.js
+++ src/models/index.js
@@ -1,23 +1,25 @@
-import Component from './component.js'
-import Config from './config.js'
-import Project from './project.js'
-import User from './user.js'
-import Sns from './sns.js'
-import ComponentType from './componentType.js'
-import Template from './template.js'
-import Log from './log.js'
-import Code from './code.js'
-import Capsule from './capsule.js'
+// import Component from './component.js'
+// import Config from './config.js'
+// import Project from './project.js'
+// import User from './user.js'
+// import Sns from './sns.js'
+// import ComponentType from './componentType.js'
+// import Template from './template.js'
+
+import mongoose from 'mongoose';
+import { MConfig } from 'schema'
+
+const db_manage = mongoose.createConnection('mongodb://root:123456@192.168.0.124:27017/manage?authSource=admin');
+
+const Config = new MConfig(db_manage);
 
 export default {
-  Component,
-  Config,
-  Project,
-  User,
-  Sns,
-  ComponentType,
-  Template,
-  Log,
-  Code,
-  Capsule,
+  Config
+  // Component,
+  // Config,
+  // Project,
+  // User,
+  // Sns,
+  // ComponentType,
+  // Template,
 }
