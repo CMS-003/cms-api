3141c0895d8fd5b3cf126aaa1a36524fb9500b5b
max <ruanjiayou123@gmail>
2024-07-18 23:33:03 +0800

    test ci

--- src/routes/v1/models/index.js
+++ src/routes/v1/models/index.js
@@ -3,6 +3,15 @@ import _ from 'lodash'
 
 const router = new Router();
 
+router.get('/', async ({ response, models }) => {
+  const tables = [];
+  Object.keys(models).forEach(table => {
+    const fields = models[table].getAttributes();
+    tables.push({ table, fields });
+  });
+  response.success(tables);
+});
+
 router.get('/:name', async ({ params, response, models }) => {
   const name = _.upperFirst(params.name)
   const fields = models[name].getAttributes();
