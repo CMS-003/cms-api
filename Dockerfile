59b1a692471dfc570ed9b6f67aed096de5909ba2
max <ruanjiayou123@gmail>
2024-07-19 09:19:12 +0800

    product

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,9 @@ COPY ./ /cms/api
 
 RUN npm install --omit=dev && npm install cross-env -g
 
-env MONGO_URL mongodb://root:123456@192.168.0.124:27017/manage?authSource=admin
+env mongo_manager_url mongodb://root:123456@192.168.0.124:27017/manager?authSource=admin
+env mongo_content_url mongodb://root:123456@192.168.0.124:27017/content?authSource=admin
+env mongo_crawler_url mongodb://root:123456@192.168.0.124:27017/crawler?authSource=admin
 
 CMD [ "npm", "start" ]
 
--- src/config/production.js
+++ src/config/production.js
@@ -2,9 +2,9 @@ export default {
   PORT: 3334,
   timezone: process.env.timezone || '+08:00',
   language: process.env.language || 'zh-CN',
-  mongo_manage_url: process.env.mongo_manage_url,
+  mongo_manage_url: process.env.mongo_manager_url,
   mongo_content_url: process.env.mongo_content_url,
-  mongo_download_url: process.env.mongo_download_url,
+  mongo_download_url: process.env.mongo_crawler_url,
   resource_api_prefix: 'novel-api:8097',
   page_public_url: 'https://u67631x482.vicp.fun',
   USER_TOKEN_SECRET: process.env.USER_TOKEN_SECRET || 'lp#yBMS0f!4IleTVnpA@'
