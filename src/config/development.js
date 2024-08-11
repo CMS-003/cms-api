ec486eef71a47e0a5de3477ad7289725c5b8c3c9
max <ruanjiayou123@gmail>
2024-08-11 18:07:51 +0800

    deploy

--- src/config/development.js
+++ src/config/development.js
@@ -2,6 +2,7 @@ export default {
   PORT: 3334,
   timezone: 'Asia/Shanghai',
   language: 'zh-CN',
+  mongo_system_url: process.env.mongo_system_url,
   mongo_manager_url: 'mongodb://root:123456@192.168.0.124:27017/manager?authSource=admin',
   mongo_content_url: 'mongodb://root:123456@192.168.0.124:27017/conetnt?authSource=admin',
   mongo_crawler_url: 'mongodb://root:123456@192.168.0.124:27017/crawler?authSource=admin',
