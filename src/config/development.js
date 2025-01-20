1b5aae346c2295afd6d67bb1afb9156fcf9cc645
阮家友 <1439120442@qq.com>
2025-01-20 11:35:25 +0800

    api proxy

--- src/config/development.js
+++ src/config/development.js
@@ -3,6 +3,7 @@ export default {
   timezone: 'Asia/Shanghai',
   language: 'zh-CN',
   mongo_system_url: 'mongodb://root:123456@127.0.0.1:27017/schema?authSource=admin',
+  proxy_host: '127.0.0.1:3333',
   mq_url: 'http://guest:guest@10.0.15.240:5672',
   resource_api_prefix: 'http://127.0.0.1:3334',
   page_public_url: 'https://u67631x482.vicp.fun',
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 import Router from 'koa-router'
+import http from 'http'
 import _ from 'lodash'
+import config from '#config/index.js';
 
 const home = new Router();
 
@@ -26,5 +28,41 @@ home.post('test/email', async (ctx) => {
     ctx.response.throwBiz('COMMON.NeedParam', { param: 'config' })
   }
 })
+
+const proxy = new Router();
+proxy.all('/(.*)', async (ctx) => {
+  const [hostname = '127.0.0.1', port = 80] = config.proxy_host.split(':');
+  const proxy_url = ctx.url.substring(10)
+  const options = {
+    hostname, // 目标服务器地址
+    port,              // 目标服务器端口
+    path: proxy_url,          // 保留原始请求路径
+    method: ctx.method,     // 保留原始请求方法
+    headers: ctx.headers    // 转发原始请求头
+  };
+  await new Promise((resolve, reject) => {
+    const proxyReq = http.request(options, (proxyRes) => {
+      // 设置返回的状态码和头部
+      ctx.status = proxyRes.statusCode;
+      ctx.set(proxyRes.headers);
+
+      // 转发数据流
+      proxyRes.pipe(ctx.res);
+      proxyRes.on('end', resolve);
+    });
+
+    proxyReq.on('error', (err) => {
+      console.error('Proxy error:', err);
+      ctx.status = 500;
+      ctx.body = 'Error forwarding request';
+      reject(err);
+    });
+
+    // 将请求体转发到目标服务器
+    ctx.req.pipe(proxyReq);
+  });
+});
+
+home.use('proxy', proxy.routes(), proxy.allowedMethods());
 
 export default home
