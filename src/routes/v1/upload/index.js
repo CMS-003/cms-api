b09e2fb887a356e478e77ce6146b3e31064b59ea
阮家友 <1439120442@qq.com>
2025-04-25 17:42:10 +0800

    图片上传

--- src/routes/v1/upload/index.js
+++ src/routes/v1/upload/index.js
@@ -5,6 +5,8 @@ import shortid from 'shortid'
 import verify from '#middleware/verify.js'
 import constant from '#constant.js'
 import mime from 'mime'
+import path from 'path'
+import dayjs from '#utils/dayjs.js'
 
 const router = new Router();
 
@@ -13,7 +15,8 @@ router.post('/image', async ({ request, response }) => {
   if (_.isArray(file)) {
     return response.fail({ code: 400, message: '不支持上传多个文件' });
   } else {
-    const filepath = `/upload/${shortid.generate()}.${mime.getExtension(file.mimetype)}`
+    const filepath = `/images/manager/${dayjs().format('YYYY-MM/DD')}/${shortid.generate()}.${mime.getExtension(file.mimetype)}`
+    fs.mkdirSync(path.dirname(constant.PATH.STATIC + filepath), { recursive: true })
     fs.renameSync(file.filepath, constant.PATH.STATIC + filepath)
     return response.success(filepath);
   }
